ff01634e3398ac6c873905bcaa321f4c179acbf5
Marko Prelevikj <pr3mar@users.noreply.github.com>
2020-02-20 21:39:06 +0100

    max_score2 is out

--- src/scores.py
+++ src/scores.py
@@ -1,5 +1,7 @@
 from statistics import mean
 
-def filter_libs(book_scores, libraries):
 
-    return libraries
+def filter_libs(book_scores, libraries):
+    mean_score = mean(book_scores)
+    filter(lambda x: mean(x["book_scores"] >= mean_score), libraries)
+    return list(libraries)
